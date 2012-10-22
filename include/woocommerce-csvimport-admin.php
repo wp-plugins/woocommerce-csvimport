01bf5eca16724ab08b313cfecef2f561421de644
allaerd <allaerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-22 21:34:14 +0000

    version 0.6.0
    * added experimental schedule function. Can be enabled for fixed imports in the settngs page.
    * solved some bugs related to empy fields in import
    * solved some bugs related to check if a file is there and if it is valid
    
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-csvimport/tags/0.5.5@615795 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/woocommerce-csvimport-admin.php
@@ -0,0 +1,196 @@
+<?php
+class woocsv_import_admin {
+
+	public function __construct() {
+		add_action('admin_menu', array($this,'admin_menu'));
+	}
+
+	public function admin_menu(){
+		//add main menu page
+		add_menu_page('CSV Import', 'CSV Import', 'manage_options', 'woocsv_import', array($this,'main_page'),'',58);
+		//add settings page
+		add_submenu_page( 'woocsv_import', 'Settings', 'Settings', 'manage_options', 'woocsv_settings', array($this,'settings'));
+	}
+
+	public function main_page(){
+	//some bassic checks
+	$upload_dir = wp_upload_dir();
+	if (!is_writable($upload_dir['basedir'].'/csvimport/'))
+		woocsv_admin_notice ('Import directory niet gevonden of hij is niet schrijfbaar. check of /uploads/csvimport bestaat');
+		
+	//handle zip uploads
+	if ( isset( $_REQUEST['handle_csv_import_zip']) && check_admin_referer('handle_csv_import_zip')) 
+		woocsv_handle_zip_import();
+	//handle manual uploads
+	if ( isset( $_REQUEST['handle_csv_import_random']) && check_admin_referer('handle_csv_import_random')) 
+		wppcsv_handle_csv_import_random();
+	//handle fixed uploads
+	if ( isset( $_REQUEST['handle_csv_import_fixed']) && check_admin_referer('handle_csv_import_fixed')) 
+		woocsv_handle_fixed_import();
+
+	
+	//main page
+		echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
+                <h2>Import</h2></div>';
+     ?>
+	<script>
+	jQuery(document).ready(function() {
+		    jQuery( "#tabs" ).tabs();
+		  });
+
+	</script>
+	<div id="tabs">
+		<ul>
+			<li><a href="#tabs-1"><?php echo __('Select a zip file'); ?></a></li>
+			<li><a href="#tabs-2"><?php echo __('Select youre own files'); ?></a></li>
+			<li><a href="#tabs-3"><?php echo __('You already uploaded the files');?></a></li>
+		</ul>
+		<div id="tabs-1">
+			<?php
+			echo '<h3>'.__('Upload a zip file').'</h3>';
+			echo '<form id="handle_csv_import_zip" name="handle_csv_import_zip" action="" method="POST" enctype="multipart/form-data">';
+			echo '<input id="zip_file" name="zip_file" type="file" accept="application/zip"> <br />';
+			echo '<input name="handle_csv_import_zip" type="submit" value="start">';
+			echo wp_nonce_field('handle_csv_import_zip');
+			echo '</form>';
+			?>
+		</div>
+		<div id="tabs-2">
+			<?php
+			echo '<h3>'.__('Upload selected files from').'</h3>';
+			echo '<p>'.__('We will only proccess csv and jpg files').'</p>';
+			echo '<form id="handle_csv_import_random" name="handle_csv_import_random" method="POST" enctype="multipart/form-data">';
+			echo __('jpg en csv:').'<input id="all_files" name="all_files[]" type="file" multiple> <br />';
+			echo '<input name="handle_csv_import_random" type="submit" value="start">';
+			echo wp_nonce_field('handle_csv_import_random');
+			echo '</form>';
+			?>
+		</div>
+		<div id="tabs-3">
+			<?php
+			echo '<h3>'.__('You already uploaded the files').'</h3>';
+			echo '<p>'.__('We expect it to be the in uploads/csvimport/fixed').'</p>';
+			echo '<form id="handle_csv_import_fixed" name="handle_csv_import_fixed" method="POST">';
+			echo __('Override fixed directory with uploads').'<input type="text" name="fixed_dir" value="/csvimport/fixed"><br />';
+			echo '<input name="handle_csv_import_fixed" type="submit" value="start">';
+			echo wp_nonce_field('handle_csv_import_fixed');
+			echo '</form>';
+			?>
+		</div>
+	</div>
+	<?php
+	}
+
+	public function settings(){
+	global $woocsv_options;
+	$upload_dir = wp_upload_dir();
+
+	//handle form for creation of import dir
+	if ( isset( $_REQUEST['create_import_directory']) && check_admin_referer('create_import_directory')) {
+		mkdir($upload_dir['basedir'] .'/csvimport/');
+		mkdir($upload_dir['basedir'] .'/csvimport/fixed/');
+	}
+	//handle form for images
+	if ( isset( $_REQUEST['delete_images_import']) && check_admin_referer('delete_images_import')) {
+		$woocsv_options['deleteimages'] = $_POST['images_import'];
+		update_option( 'csvimport-options', $woocsv_options );
+	}
+	
+	//hanlde form for fields seperator
+	if ( isset( $_REQUEST['fieldseperator']) && check_admin_referer('fieldseperator')) {
+		$woocsv_options['fieldseperator'] = $_POST['fieldseperatorvalue'];
+		update_option( 'csvimport-options', $woocsv_options );
+	}	
+	
+	//hanlde form for auto detect line endings
+	if ( isset( $_REQUEST['auto_detect_line_endings']) && check_admin_referer('auto_detect_line_endings')) {
+		$woocsv_options['auto_detect_line_endings'] = $_POST['auto_detect_line_endings'];
+		update_option( 'csvimport-options', $woocsv_options );
+	}
+	
+	//hanlde form for change comma to dot
+	if ( isset( $_REQUEST['change_comma_to_dot']) && check_admin_referer('change_comma_to_dot')) {
+		$woocsv_options['change_comma_to_dot'] = $_POST['change_comma_to_dot'];
+		update_option( 'csvimport-options', $woocsv_options );
+	}
+
+	if (!is_writable($upload_dir['basedir'].'/csvimport/'))
+		woocsv_admin_notice ('Import directory niet gevonden of hij is niet schrijfbaar. check of /uploads/csvimport bestaat');
+		
+		
+		echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
+                <h2>Settings</h2></div>';
+        
+        //import directory
+        echo '<h3>Create import directory</h3>';                        
+        if (!is_dir($upload_dir['basedir'] .'/csvimport/')) {
+			echo '<form id="create_import_directory" name="create_import_directory" method="POST">';
+			echo '<input name="create_import_directory" type="submit" value="create">';
+			echo wp_nonce_field('create_import_directory');
+			echo '</form>';
+	        
+        }
+	    echo '<p>Directory is there!</p>';
+	  
+	    //what to do with images
+	    ?>
+	    <h3>What to do with images?</h3>
+	    <p>You can choose if you want to keep the existing images or delete them before adding the new ones.</p>
+	    <form id="delete_images_import" name="delete_images_import" method="POST">
+	    <select id="images_import" name ="images_import">
+	    <option value=0 <?php if ($woocsv_options['deleteimages'] == 0) echo 'selected'; ?> >append images to product</option>
+	    <option value=1 <?php if ($woocsv_options['deleteimages'] == 1) echo 'selected'; ?> >remove all images before uploading new ones</option>
+	    </select>
+		<input name="delete_images_import" type="submit" value="Save">
+		<?php echo wp_nonce_field('delete_images_import'); ?>
+		</form>
+		
+		<h3>What to do with line endings?</h3>
+	    <p>If you have problems with the line endings. try the option "use auto select"</p>
+	    <form id="auto_detect_line_endings" name="auto_detect_line_endings" method="POST">
+	    <select id="auto_detect_line_endings" name ="auto_detect_line_endings">
+	    <option value=0 <?php if ($woocsv_options['auto_detect_line_endings'] == 0) echo 'selected'; ?> >use default</option>
+	    <option value=1 <?php if ($woocsv_options['auto_detect_line_endings'] == 1) echo 'selected'; ?> >use auto detect</option>
+	    </select>
+		<input name="" type="submit" value="Save">
+		<?php echo wp_nonce_field('auto_detect_line_endings'); ?>
+		</form>
+		
+		<h3>What to do with the field seperator?</h3>
+	    <p>You can choose what field seperator you want to use</p>
+	    <form id="fieldseperator" name="fieldseperator" method="POST">
+		<input type="text" name="fieldseperatorvalue" id="fieldseperatorvalue" size="1" value="<?php echo $woocsv_options['fieldseperator']; ?>">
+		<input name="fieldseperator" type="submit" value="Save">
+		<?php echo wp_nonce_field('fieldseperator'); ?>
+		</form>
+		
+		<h3>What to do with comma's that are in the price fields?</h3>
+	    <p>You can do nothing or convert them to dot's</p>
+	    <form id="change_comma_to_dot" name="change_comma_to_dot" method="POST">
+		<select id="change_comma_to_dot" name ="change_comma_to_dot">
+	    <option value=0 <?php if ($woocsv_options['change_comma_to_dot'] == 0) echo 'selected'; ?> >do nothing</option>
+	    <option value=1 <?php if ($woocsv_options['change_comma_to_dot'] == 1) echo 'selected'; ?> >replace to dot</option>
+	    </select>
+		<input name="" type="submit" value="Save">
+		<?php echo wp_nonce_field('change_comma_to_dot'); ?>
+		</form>
+		
+		
+		<h3>How does the CSV look like?</h3>
+		<p>It looks like this:</p>
+		<pre>
+		<code>
+title,description,short_description,category,stock,price,regular_price,sales_prices,weight,length,width,height,sku,picture,tags,tax_status,tax_class
+product1,very nice product,nice product,cat1->subcat1->subsubcat1|cat2,10,100,100,90,2,1,2,3,123456789,product1.jpg|product2.jpg|product3.jpg,tag1|tag2|tag3,taxable,zero-rate
+product2,very nice product two,nice product two,cat2,5,200,200,190,2,4,5,6,46345,product1.jpg,tag1|tag4,taxable,zero-rate
+product3,very nice product three with strange letters čšžýáí,nice product three,cat2,5,200,200,190,2,4,5,6,98765,http://www.allaerd.org/plugins/product4.jpg,tag2|tag5,taxable,zero-rate
+</code>
+		</pre>
+		<?php
+    }
+    
+
+
+}
+
+$csv_import_admin = new woocsv_import_admin();
---
+++ include/woocommerce-csvimport-functions.php
@@ -0,0 +1,321 @@
+<?php
+function wppcsv_handle_csv_import_random () {
+	//try {
+	//create temp directory
+	$upload_dir = wp_upload_dir();
+	$dir = $upload_dir['basedir'] .'/csvimport/' . woocsv_random_string() . '/';
+	mkdir($dir);
+	//handle upload
+	if (!woocsv_handle_uploads ($dir))
+		throw new Exception(__('Er is iets mis gegaan bij het uploaden'));
+
+	$are_there_csv_files = glob($dir.'*.csv');
+	if (count($are_there_csv_files) != 1) throw new Exception(__('Er is geen of meerdere csv bestand(en) gevonden'));
+	//run the magic
+	$result = woocsv_import_products_from_csv ($are_there_csv_files[0],$dir);
+
+	//}  catch (Exception $e) {
+	// woocsv_admin_notice ($e->getMessage());
+	//}
+
+}
+
+
+function woocsv_handle_zip_import () {
+	//create temp directory
+	try {
+		$upload_dir = wp_upload_dir();
+		$dir = $upload_dir['basedir'] .'/' . woocsv_random_string() . '/';
+		mkdir($dir);
+		$file = $_FILES['zip_file']['name'];
+		move_uploaded_file( $_FILES['zip_file']['tmp_name'], $dir.$file );
+		$zip = new ZipArchive;
+		if ($zip->open($dir.$file) === TRUE) {
+			$zip->extractTo($dir);
+			$zip->close();
+		} else throw new Exception(__('Kan de zip file niet uitpakken'));
+
+		$are_there_csv_files = glob($dir.'*.csv');
+		if (count($are_there_csv_files) != 1) throw new Exception(__('Er is geen of meerdere csv bestand(en) gevonden'));
+		//run the magic
+		$result = woocsv_import_products_from_csv ($are_there_csv_files[0],$dir);
+	}
+	catch (Exception $e) {
+		woocsv_admin_notice ($e->getMessage());
+	}
+
+}
+
+function woocsv_handle_fixed_import () {
+	//get the upload dir
+	$upload_dir = wp_upload_dir();
+	$dir = $upload_dir['basedir'] .$_POST['fixed_dir'] .'/';
+	try {
+		//check the existence of the directory
+		if (!glob($dir)) throw new Exception(__('De directory bestaat niet'));
+
+		//check to see if there are files in
+		if ( count( scandir( $dir ) ) <= 2) throw new Exception(__('Er zijn geen bestanden aanwezig in de directory'));
+
+		//now check to see if there is a csv in there
+		$are_there_csv_files = glob($dir.'*.csv');
+		if (count($are_there_csv_files) != 1) throw new Exception(__('Er is geen of meerdere csv bestand(en) gevonden'));
+
+		//run the magic
+		$result = woocsv_import_products_from_csv ($are_there_csv_files[0],$dir);
+
+
+	} catch (Exception $e) {
+		woocsv_admin_notice ($e->getMessage());
+	}
+}
+
+//import the products
+function woocsv_import_products_from_csv ($file,$dir) {
+	global $wpdb, $woocsv_options;
+	$fieldseperator = (isset($woocsv_options['fieldseperator'])) ?  $woocsv_options['fieldseperator'] : ',';
+	set_time_limit(0);
+	$row = 0;
+	if ( $woocsv_options['auto_detect_line_endings'] == 1 ) 
+		ini_set('auto_detect_line_endings', true);
+	if ($handle = fopen($file, 'r') == FALSE) throw new Exception(__('Can not open file!'));
+	$handle = fopen($file, 'r');
+	$csvcontent = '';
+	while (($line = fgetcsv($handle,0,$woocsv_options['fieldseperator'])) !== FALSE) {
+		if ($row <> 0 ) $csvcontent[] = $line;
+		$row ++;
+	}
+	fclose($handle);
+	
+	if (!$csvcontent) {
+		woocsv_admin_notice('No content in csv....check it (also the line endings!)');
+		exit;
+	}
+		
+	$content = $csvcontent;
+	/*
+	0 title,
+	1 description,
+	2 short_description,
+	3 category
+	4 stock,
+	5 price,
+	6 regular_price,
+	7 sales_price,
+	8 weight,
+	9 length,
+	10 width,
+	11 height,
+	12 sku,
+	13 picture
+	14 tags
+	15 tax status ( taxable, shipping, none )
+	16 tax class 
+	*/
+	foreach ( $content as $data ) {
+		$num = count($data);
+		$row ++;
+		$my_product = array(
+			'post_title' => wp_strip_all_tags( $data[0] ),
+			'post_content' => $data[1],
+			'post_excerpt' => $data[2],
+			'post_status' => 'publish' ,
+			'post_type' => 'product'
+		);
+		//check to see if the product already exists and add the ID if true
+		$product_id = $wpdb->get_var($wpdb->prepare("SELECT post_id FROM $wpdb->postmeta
+				WHERE meta_key='_sku' AND meta_value='%s' LIMIT 1", $data[12] ));
+		if ($product_id) $my_product['ID'] = $product_id;
+		//now we create the product...ig the id is there is will update the product else it will make a new
+		$post_id = wp_update_post($my_product);
+
+		//set the attributes etc
+		if ( $data[4] ) 
+			update_post_meta( $post_id, '_stock', $data[4] );
+
+		//set the price and replace , by . if set 
+		if ( $data[5] ) {
+			if ($woocsv_options['change_comma_to_dot'] == 1) $data[5] = str_replace(',', '.', $data[5]);
+			update_post_meta( $post_id, '_price', $data[5] );
+		}
+
+		if ( $data[6] ) {
+			if ($woocsv_options['change_comma_to_dot'] == 1) $data[6] = str_replace(',', '.', $data[6]);
+			update_post_meta( $post_id, '_regular_price', $data[6] );
+		}
+
+		if ( $data[7] ) {
+			if ($woocsv_options['change_comma_to_dot'] == 1) $data[7] = str_replace(',', '.', $data[7]);
+			update_post_meta( $post_id, '_sale_price', $data[7] );
+		}
+		//end prices
+
+		//set the weight
+		if ($data[8]) 
+			update_post_meta( $post_id, '_weight', $data[8] );
+
+		//set the length 
+		if ($data[9])
+			update_post_meta( $post_id, '_length', $data[9] );
+
+		//set the height 
+		if ( $data[10] )
+			update_post_meta( $post_id, '_width', $data[10] );
+
+		//set the height 
+		if ( $data[11] )
+			update_post_meta( $post_id, '_height', $data[11] );
+
+		//set the SKU
+		if ( $data[12] ) 
+			update_post_meta( $post_id, '_sku', $data[12] );
+
+		update_post_meta( $post_id, '_manage_stock', 'yes' );
+		update_post_meta( $post_id, '_visibility', 'visible' );
+		
+		//tax status taxable, shipping, none
+		$tax_status = array ('taxable', 'shipping', 'none');
+		if ($data[15]) {
+			//check if the data is in the array
+			if (in_array($data[15], $tax_status))
+				update_post_meta( $post_id, '_tax_status', $data[15] );
+		}
+		
+		//tax class
+		if ($data[16])
+			update_post_meta( $post_id, '_tax_class', $data[16] );
+		
+		//link the product to the category
+		$cats = explode ( '|', $data[3] );
+		foreach ($cats as $cat){
+			$cat_taxs = explode( '->', $cat );
+			$parent = false;
+			foreach ( $cat_taxs as $cat_tax)
+			{
+				$new_cat = term_exists( $cat_tax, 'product_cat' );
+				if ( ! is_array( $new_cat ) ) {
+					$new_cat = wp_insert_term(	$cat_tax, 'product_cat', array( 'slug' => $cat_tax, 'parent'=> $parent) );
+				}
+				wp_set_object_terms( $post_id, (int)$new_cat['term_id'], 'product_cat', true );
+				$parent = $new_cat['term_id'];
+				
+				//check out http://wordpress.stackexchange.com/questions/24498/wp-insert-term-parent-child-problem
+				delete_option("product_cat_children");
+			}
+			unset($parent);	
+		}
+
+		//handle tags
+		if ( isset( $data[14] )){
+			$tags = explode('|', $data[14]);
+			
+			wp_set_object_terms( $post_id, $tags, 'product_tag',true );			
+		}
+
+		//get picture if there is one and add it as featured image
+		if ( isset( $data[13] )) {
+			woocsv_add_featured_image ( $post_id , $data[13], $dir );
+		}
+	}
+}
+
+function woocsv_add_featured_image($post_id,$image_array,$dir) {
+	$options = get_option('csvimport-options');
+	$upload_dir = wp_upload_dir();
+	//delete images
+	if ($options['deleteimages'] == 1) {
+		//get the images
+		$attachments = get_posts( array(
+				'post_type' => 'attachment',
+				'post_parent' => $post_id,
+			));
+		foreach ($attachments as $attachment) {
+			wp_delete_attachment ($attachment->ID);
+		}
+	}
+
+	$images = explode('|', $image_array);
+	if (count($images) > 0) {
+		foreach ($images as $image) {
+			if ( woocsv_isvalidurl( $image ) ) {
+				$image_data = file_get_contents($image); 
+			} else {
+				$image_data = file_get_contents($dir.$image);
+			}
+
+			if ( $image_data ) {
+				$filename = basename($image);
+				if(wp_mkdir_p($upload_dir['path']))
+					$file = $upload_dir['path'] . '/' . $filename;
+				else
+					$file = $upload_dir['basedir'] . '/' . $filename;
+
+				if (file_put_contents($file, $image_data)) {
+					$wp_filetype = wp_check_filetype($filename, null );
+					$attachment = array(
+						'post_mime_type' => $wp_filetype['type'],
+						'post_title' => sanitize_file_name($filename),
+						'post_content' => '',
+						'post_status' => 'inherit'
+					);
+
+					$attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+					require_once(ABSPATH . 'wp-admin/includes/image.php');
+					$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+					wp_update_attachment_metadata( $attach_id, $attach_data );
+					set_post_thumbnail( $post_id, $attach_id );
+
+				}
+			}
+		}
+	}
+
+}
+
+function woocsv_admin_notice($message=''){
+	if ($message)
+		echo '<div class="error"><p>'.$message.'</p></div>';
+}
+
+//well this doed what is does....create a reandom string
+function woocsv_random_string($length = 10) {
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW';
+	$string = '';
+	for ($p = 0; $p < $length; $p++) {
+		$string .= $characters[mt_rand(0, strlen($characters))];
+	}
+	return $string;
+}
+
+function woocsv_get_normalized_files()
+{
+	$newfiles = array();
+	foreach($_FILES as $fieldname => $fieldvalue)
+		foreach($fieldvalue as $paramname => $paramvalue)
+			foreach((array)$paramvalue as $index => $value)
+				$newfiles[$fieldname][$index][$paramname] = $value;
+			return $newfiles;
+}
+
+
+
+//handle file uploads
+function woocsv_handle_uploads ( $dir ){
+	try {
+		$files = woocsv_get_normalized_files();
+		foreach ($files['all_files'] as $file) {
+			$from_location = $file['tmp_name'];
+			$to_location = $dir . $file['name'];
+			//check if file is csv or jpg
+			move_uploaded_file($from_location, $to_location);
+		}
+		return true;
+	} catch (MyException $e) {
+		return false;
+	}
+}
+
+function woocsv_isvalidurl($url)
+{
+return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
+}
