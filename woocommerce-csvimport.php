3f0448a44774832df73544496406656841b1ab00
allaerd <allaerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-08 08:21:13 +0000

    version 0.7.2
    change menu order to have less convicts with other plugins
    
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-csvimport/trunk@665099 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-csvimport.php
+++ woocommerce-csvimport.php
@@ -3,7 +3,7 @@
 Plugin Name: Woocommerce CSV Import
 Plugin URI: http://allaerd.org/woocommerce-csv-importer/
 Description: Import CSV files in Woocommerce
-Version: 0.7.1
+Version: 0.7.2
 Author: Allaerd Mensonides
 License: GPLv2 or later
 Author URI: http://allaerd.org
