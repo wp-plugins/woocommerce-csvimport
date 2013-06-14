5acae2a8f848f307ab25c719078ff30242a26262
allaerd <allaerd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-14 20:13:13 +0000

    version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-csvimport/trunk@726787 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/woocsv.js
@@ -0,0 +1,81 @@
+var d = Date.now();
+jQuery(document).ready(function()
+{
+	jQuery('#headerForm').submit(function(e)
+	{
+		var saveHeaderForm = jQuery(this).serialize();
+		jQuery.ajax(
+		{
+			type: "POST",
+			url: "/wp-admin/admin-ajax.php",
+			data: saveHeaderForm,
+			success: function(data)
+			{
+				jQuery('html, body').animate(
+				{
+					scrollTop: 0
+				}, 'slow');
+				jQuery('#woocsv_warning').html(data);
+				jQuery("#woocsv_warning").slideDown().delay(2500).slideUp();
+			}
+		});
+		e.preventDefault();
+	});
+	
+	jQuery('#settingsForm').submit(function(e)
+	{
+		var formData = jQuery(this).serialize();
+		jQuery.ajax(
+		{
+			type: "POST",
+			url: "/wp-admin/admin-ajax.php",
+			data: formData,
+			success: function(data)
+			{
+				jQuery('#woocsv_warning').html(data);
+				jQuery("#woocsv_warning").slideDown().delay(2500).slideUp();
+			}
+		});
+		e.preventDefault();
+	});
+	jQuery('#runImportForm').submit(function(e)
+	{
+		jQuery('html, body').animate(
+		{
+			scrollTop: 0
+		}, 'slow');
+		jQuery('#importPreview').slideUp();
+		var formData = jQuery(this).serialize();
+		doAjaxImport(formData);
+		e.preventDefault();
+	});
+});
+
+function doAjaxImport(formData)
+{
+	jQuery.ajax(
+	{
+		type: "POST",
+		url: "/wp-admin/admin-ajax.php",
+		data: formData,
+		success: function(data)
+		{ 
+			console.log(data);
+			if (data != 'done')
+			{
+				var newFormData = jQuery.parseJSON(data);
+				jQuery('#import_log').prepend('<p> row: ' + newFormData.currentrow + ' / ' + newFormData.rows + ' product id = ' + newFormData.ID + ' product sku = ' + newFormData.sku + '</p>');
+				doAjaxImport(newFormData);
+			}
+			else
+			{
+				jQuery('#import_log').prepend('<p>Done!</p>');
+			}
+		},
+		error: function(data)
+		{
+		console.log(data);
+			alert('something went wrong');
+		}
+	});
+}
